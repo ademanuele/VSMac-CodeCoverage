97b804fdf404da499541865e2826f5ae382727ec
Arthur Demanuele <ademan19@gmail.com>
2020-02-22 16:13:25 +0100

    Continue margin implementation

--- VSMac-CodeCoverage/VSMac-CodeCoverage/EditorExtension/CodeCoverageMargin.cs
+++ VSMac-CodeCoverage/VSMac-CodeCoverage/EditorExtension/CodeCoverageMargin.cs
@@ -1,33 +1,19 @@
-﻿using System;
-using AppKit;
+﻿using AppKit;
 using CodeCoverage.Coverage;
-using CoreGraphics;
-using Foundation;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using MonoDevelop.Ide;
 using MonoDevelop.Projects;
 using System.Collections.Generic;
 using System.Diagnostics;
-using ICSharpCode.NRefactory.MonoCSharp;
 
 namespace CodeCoverage
 {
-  class CodeCoverageMargin : NSView, ICocoaTextViewMargin
+  class CodeCoverageMargin : ICocoaTextViewMargin
   {
-    public double MarginSize => MarginWidth;
-    public NSView VisualElement => this;
+    public double MarginSize => 25.0f;
+    public NSView VisualElement => marginView;
     public bool Enabled => true;
-
-    private const float MarginWidth = 20.0f;
-    private static readonly CGColor CoveredColor = new CGColor(0.54f, 0.97f, 0.57f);
-    private static readonly CGColor UncoveredColor = new CGColor(0.97f, 0.57f, 0.54f);
-
-    private readonly ITextView textView;
-    private Dictionary<int, int> coverage;
-
-    public override bool IsFlipped => true;
-    public override CGSize IntrinsicContentSize => new CGSize(MarginWidth, NoIntrinsicMetric);
 
     private CoveragePadWidget CoveragePadWidget
     {
@@ -38,9 +24,13 @@ namespace CodeCoverage
       }
     }
 
+    private readonly ITextView textView;
+    private readonly CodeCoverageMarginView marginView;
+
     public CodeCoverageMargin(ITextView textView)
     {
       this.textView = textView;
+      marginView = new CodeCoverageMarginView(textView, MarginSize);
       this.textView.LayoutChanged += OnTextViewLayoutChanged;
       CoveragePadWidget.SelectedTestProjectChanged += HandleSelectedTestProjectChanged;
       UpdateCoverage();
@@ -52,7 +42,7 @@ namespace CodeCoverage
     {
       Debug.WriteLine("Updating Coverage for Margin...");
       if (!TryGetCoverageFor(textView, out var results)) return;
-      coverage = results;
+      marginView.Coverage = results;
     }
 
     bool TryGetCoverageFor(ITextView textView, out Dictionary<int, int> coverage)
@@ -79,49 +69,15 @@ namespace CodeCoverage
       return document.FilePath;
     }
 
-    public override void DrawRect(CGRect dirtyRect)
-    {
-      if (coverage == null) return;
+    private void OnTextViewLayoutChanged(object sender, TextViewLayoutChangedEventArgs e) => marginView.NeedsDisplay = true;
 
-      var context = NSGraphicsContext.CurrentContext.CGContext;
-      context.SetLineWidth(MarginWidth);
+    public ITextViewMargin GetTextViewMargin(string marginName) => marginName == nameof(CodeCoverageMargin) ? this : null;
 
-      foreach (var line in textView.TextViewLines)
-      {
-        var lineNumber = line.Start.GetContainingLine().LineNumber + 1;
-        if (!coverage.TryGetValue(lineNumber, out int visitCount)) continue;
-
-        context.SetFillColor(visitCount > 0 ? CoveredColor : UncoveredColor);
-        var rect = new CGRect(0, line.Top, MarginWidth, line.Height);
-        context.FillRect(rect);
-
-        var attrs = new NSStringAttributes();
-        var font = NSFont.SystemFontOfSize(11f);
-        attrs.Font = font;
-
-        var fontHeight = font.PointSize;
-        var yOffset = (rect.Size.Height - fontHeight) / 2.0;
-        CGRect textRect = new CGRect(0, rect.Y - yOffset, rect.Size.Width, fontHeight);
-
-        var coverageString = new NSString(visitCount.ToString());
-        coverageString.DrawString(textRect, new NSDictionary());
-      }
-    }
-
-    private void OnTextViewLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
-    {
-      NeedsDisplay = true;
-    }
-
-    public ITextViewMargin GetTextViewMargin(string marginName)
-    {
-      return marginName == nameof(CodeCoverageMargin) ? this : null;
-    }
-
-    protected override void Dispose(bool disposing)
+    public void Dispose()
     {
       textView.LayoutChanged -= OnTextViewLayoutChanged;
       CoveragePadWidget.SelectedTestProjectChanged -= HandleSelectedTestProjectChanged;
+      marginView.Dispose();
     }
   }
 }
---
+++ VSMac-CodeCoverage/VSMac-CodeCoverage/EditorExtension/CodeCoverageMarginView.cs
@@ -0,0 +1,77 @@
+﻿using AppKit;
+using CoreGraphics;
+using Foundation;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using System.Collections.Generic;
+
+namespace CodeCoverage
+{
+  class CodeCoverageMarginView : NSView
+  {
+    public Dictionary<int, int> Coverage
+    {
+      get => coverage;
+      set
+      {
+        coverage = value;
+        NeedsDisplay = true;
+      }
+    }
+
+    private static readonly CGColor CoveredColor = new CGColor(0.18f, 0.49f, 0.20f);
+    private static readonly CGColor UncoveredColor = new CGColor(0.49f, 0.20f, 0.18f);
+    private const float HorizontalPadding = 2f;
+
+    private readonly ITextView textView;
+    private readonly double width;
+    private Dictionary<int, int> coverage;
+
+    public CodeCoverageMarginView(ITextView textView, double width)
+    {
+      this.textView = textView;
+      this.width = width;
+    }
+
+    public override bool IsFlipped => true;
+    public override CGSize IntrinsicContentSize => new CGSize(width, NoIntrinsicMetric);
+
+    public override void DrawRect(CGRect dirtyRect)
+    {
+      if (coverage == null) return;
+      var context = NSGraphicsContext.CurrentContext.CGContext;
+
+      foreach (var line in textView.TextViewLines)
+      {
+        var lineNumber = line.Start.GetContainingLine().LineNumber + 1;
+        if (!coverage.TryGetValue(lineNumber, out int visitCount)) continue;
+        DrawCoverageForLine(line, visitCount, context);
+      }
+    }
+
+    void DrawCoverageForLine(ITextViewLine line, int visitCount, CGContext context)
+    {
+      var top = line.Top - textView.ViewportTop;
+      var rect = new CGRect(0, top, width, line.Height);
+
+      context.SetFillColor(visitCount > 0 ? CoveredColor : UncoveredColor);
+      context.FillRect(rect);
+
+      var attrs = new NSStringAttributes();
+      var fontSize = (float)line.Height - 2f;
+      var font = NSFont.MonospacedDigitSystemFontOfSize(fontSize, 1); // MAKE MONOSPACED
+      attrs.Font = font;
+
+      var yOffset = (rect.Size.Height - fontSize) / 2.0;
+
+      CGRect textRect = new CGRect(HorizontalPadding, rect.Y + yOffset, rect.Size.Width - (HorizontalPadding * 2), fontSize);
+
+      var coverageString = new NSString(visitCount.ToString());
+      var stringAttributes = new NSStringAttributes
+      {
+        ForegroundColor = NSColor.White,
+      };
+      coverageString.DrawInRect(textRect, stringAttributes);
+    }
+  }
+}
