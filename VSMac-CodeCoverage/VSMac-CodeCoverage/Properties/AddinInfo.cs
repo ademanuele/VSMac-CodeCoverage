63dd3a5a32b0a806e848875ca8b5c647098fbc42
Arthur Demanuele <ademan19@gmail.com>
2021-05-03 20:59:16 +0200

    update version

--- VSMac-CodeCoverage/VSMac-CodeCoverage/Properties/AddinInfo.cs
+++ VSMac-CodeCoverage/VSMac-CodeCoverage/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Code Coverage",
     Namespace = "CodeCoverage",
-    Version = "1.2"
+    Version = "1.3"
 )]
 
 [assembly: AddinName("CodeCoverage")]
